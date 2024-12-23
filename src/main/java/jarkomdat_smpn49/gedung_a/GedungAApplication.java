2fe747922856e03e530e6a8326a77b11a14e4320
thirzq <thirzahmad@gmail.com>
2024-12-23 16:33:13 +0700

    mungkin

---
+++ src/main/java/jarkomdat_smpn49/gedung_a/GedungAApplication.java
@@ -0,0 +1,13 @@
+package jarkomdat_smpn49.gedung_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedungAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedungAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jarkomdat_smpn49/gedung_a/GedungAApplicationTests.java
@@ -0,0 +1,13 @@
+package jarkomdat_smpn49.gedung_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedungAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
